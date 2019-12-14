ae170d84488ea8982c784df9b30314d559a0b8f9
Gino Carignano <gino.jc@hotmail.com>
2019-12-14 14:28:55 -0300

    App de introduccion terminada

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Inicio from './Inicio';
+import Body from './Body';
+import Final from './Final';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      numero: 3
+    }
+  }
+
+  aumentar = () => {
+    this.setState({
+      numero: this.state.numero + 1 
+    })
+  }
+
+  restar = () => {
+    this.setState({
+      numero: this.state.numero - 1 
+    })
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Inicio name="Calculador"/>
+        <Body numero={this.state.numero}/>
+        {/*<View style={styles.cajaTres}>
+          <Button title="Aumentar" onPress={() => {this.aumentar();}}/>
+          <Button title="Restar" onPress={() => {this.restar();}}/>
+        </View>*/}
+        <Final sumar={this.aumentar} restar={this.restar}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  cajaUno:{
+    flex: 1,
+    backgroundColor: '#00ff00',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  cajaDos:{
+    flex: 3,
+    backgroundColor: '#90EE90',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  cajaTres:{
+    flex: 1,
+    backgroundColor: '#006400',
+  }
 });
+
+export default App;
